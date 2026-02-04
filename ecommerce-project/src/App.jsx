5230d17a9b74562c9ea6a8e28f8c43c4abc251e6
Mario Spasovski <mario.spaska@gmail.com>
2026-02-04 21:42:34 +0100

    Separate components and related files in different folders

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,9 @@
 // import { useState } from 'react'
 import { Routes,Route } from 'react-router'
-import { HomePage } from './pages/HomePage.jsx'
-import { CheckoutPage } from './pages/CheckoutPage.jsx'
-import { OrdersPage } from './pages/OrdersPage.jsx'
-import { TrackingPage } from './pages/TrackingPage.jsx'
+import { HomePage } from './pages/home/HomePage.jsx'
+import { CheckoutPage } from './pages/checkout/CheckoutPage.jsx'
+import { OrdersPage } from './pages/orders/OrdersPage.jsx'
+import { TrackingPage } from './pages/tracking/TrackingPage.jsx'
 
 
 import './App.css'
