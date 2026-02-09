02b83f2e11403b1d07a5dc8685483cd5f83f3b80
Mario Spasovski <mario.spaska@gmail.com>
2026-02-09 16:10:39 +0100

    Convert money utils and Header to TypeScript

--- ecommerce-project/src/components/Header.tsx
+++ ecommerce-project/src/components/Header.tsx
@@ -6,7 +6,14 @@ import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import { useState } from 'react';
 
-export function Header({ cart }) {
+type HeaderProps={
+    cart:{
+        productId:string,
+        quantity:number,
+        deliveryOptionId:string;
+    }[];}
+
+export function Header({ cart }:HeaderProps) {
     const navigate = useNavigate();
     const [searchParams] = useSearchParams();
 
@@ -18,7 +25,7 @@ export function Header({ cart }) {
     // it will use a default value of ''.
     const [search, setSearch] = useState(searchText || '');
 
-    const updateSearchInput = (event) => {
+    const updateSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(event.target.value);
     };
 
--- ecommerce-project/src/utils/money.ts
+++ ecommerce-project/src/utils/money.ts
@@ -1,4 +1,4 @@
-export function formatMoney(cents) {
+export function formatMoney(cents:number):string {
     if (cents < 0) {
         return `-$${(-cents / 100).toFixed(2)}`;
     }
